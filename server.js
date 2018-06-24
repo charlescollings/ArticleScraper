cc8c3008e53c1e4dc3ea7e2c2e6d12c395cf78a6
Charles <charlescollings@usa.net>
2018-06-23 21:29:52 -0500

    sat push

---
+++ server.js
@@ -0,0 +1,143 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost:27017/articlescraper");
+
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("http://nytimes.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+  // axios.get("http://www.fantasypros.com/mlb/player-news").then(function(response) {
+  //   // Then, we load that into cheerio and save it to $ for a shorthand selector
+  //   console.log(response)
+  //   var $ = cheerio.load(response.data);
+  //   console.log($)
+    // Now, we grab every h2 within an article tag, and do the following:
+    var articles = [];
+
+    // Now, find and loop through each element that has the "theme-summary" class
+    // (i.e, the section holding the articles)
+    $(".theme-summary").each(function(i, element) {
+      // In each .theme-summary, we grab the child with the class story-heading
+
+      // Then we grab the inner text of the this element and store it
+      // to the head variable. This is the article headline
+      var head = $(this)
+        .children(".story-heading")
+        .text()
+        .trim();
+
+      // Grab the URL of the article
+      var url = $(this)
+        .children(".story-heading")
+        .children("a")
+        .attr("href");
+
+      // Then we grab any children with the class of summary and then grab it's inner text
+      // We store this to the sum variable. This is the article summary
+      var sum = $(this)
+        .children(".summary")
+        .text()
+        .trim();
+
+      // So long as our headline and sum and url aren't empty or undefined, do the following
+      if (head && sum && url) {
+        // This section uses regular expressions and the trim function to tidy our headlines and summaries
+        // We're removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
+        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+        // Initialize an object we will push to the articles array
+
+        var dataToAdd = {
+          headline: headNeat,
+          summary: sumNeat,
+          url: url
+        };
+
+        articles.push(dataToAdd);
+       
+      }
+    });
+    return articles;
+    console.log(articles)
+
+    db.Article.create(articles)
+    .then(function(dbArticle) {
+      // View the added result in the console
+      console.log(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      return res.json(err);
+    });
+     // // Add the text and href of every link, and save them as properties of the result object
+     // result.headline = $(this)
+     //   .children("a")
+     //   .text();
+     // result.link = $(this)
+     //   .children("a")
+     //   .attr("href");
+     // result.summary = $(this)
+     //   .children("p")
+     //   .text();
+//
+     // // Create a new Article using the `result` object built from scraping
+
+    // });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
