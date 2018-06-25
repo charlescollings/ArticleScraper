7585aa890508720e53532505e1101f270599ffca
Charles <charlescollings@usa.net>
2018-06-25 18:17:21 -0500

    mLab_setUp

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -27,15 +27,16 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/articleScraper");
-let uri = 'mongodb://heroku_zcq38hst:4ocitibhse46b7ub9hn3b3sk0u@ds117711.mlab.com:17711/heroku_zcq38hst';
+// 
+// let uri = 'mongodb://heroku_zcq38hst:4ocitibhse46b7ub9hn3b3sk0u@ds117711.mlab.com:17711/heroku_zcq38hst';
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleScraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 // mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI);
-mongoose.connect(uri);
+mongoose.connect(MONGODB_URI);
+// mongoose.connect(uri);
 
 // Routes
 // A GET route for scraping the echoJS website
